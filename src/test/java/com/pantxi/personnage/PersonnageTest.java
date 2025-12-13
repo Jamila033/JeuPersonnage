b106f385a126c2ac8448ed3a3d04633ace4066aa
dell <jamilaallali2003@gmail.com>
2025-12-14 00:02:09 +0100

    TDD: Implémentation complète de la méthode Personnage.tourner()

--- src/test/java/com/pantxi/personnage/PersonnageTest.java
+++ src/test/java/com/pantxi/personnage/PersonnageTest.java
@@ -6,9 +6,44 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PersonnageTest {
 
     @Test
+    void Orientation_NORD_Zero_Tour() {
+        Personnage monPersonnage = new Personnage();
+        Orientation or = monPersonnage.tourner(0);
+        assertThat(or).isEqualTo(Orientation.NORD);
+    }
+
+    @Test
+    void Orientation_EST() {
+        Personnage monPersonnage = new Personnage();
+        Orientation or = monPersonnage.tourner(1);
+        assertThat(or).isEqualTo(Orientation.EST);
+    }
+
+    @Test
     void Orientation_SUD() {
         Personnage monPersonnage = new Personnage();
         Orientation or = monPersonnage.tourner(2);
         assertThat(or).isEqualTo(Orientation.SUD);
+    }
+
+    @Test
+    void Orientation_OUEST() {
+        Personnage monPersonnage = new Personnage();
+        Orientation or = monPersonnage.tourner(3);
+        assertThat(or).isEqualTo(Orientation.OUEST);
+    }
+
+    @Test
+    void Orientation_NORD_Tour_Complet() {
+        Personnage monPersonnage = new Personnage();
+        Orientation or = monPersonnage.tourner(4);
+        assertThat(or).isEqualTo(Orientation.NORD);
+    }
+
+    @Test
+    void Orientation_EST_Plus_Un_Tour() {
+        Personnage monPersonnage = new Personnage();
+        Orientation or = monPersonnage.tourner(5);
+        assertThat(or).isEqualTo(Orientation.EST);
     }
 }
